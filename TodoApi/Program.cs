7264253e722c72a0515c929dec5a22e1d754455d
chayaziv <chy15658@gmail.com>
2025-02-06 20:14:58 +0200

    connectionString

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,21 +6,23 @@ using TodoApi;
 var builder = WebApplication.CreateBuilder(args);
 
 
-var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
+// var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
+// Console.WriteLine($"Connection String: {connectionString}");
+
+// קבלת ה-connection string ממשתנה סביבה
+var connectionString = Environment.GetEnvironmentVariable("ToDoDB");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("The database connection string is not set.");
+}
+
 Console.WriteLine($"Connection String: {connectionString}");
+
+
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 34))));
-
-
-// builder.Services.AddCors(options =>
-// {
-//     options.AddDefaultPolicy(policy =>
-//     {
-//         policy.AllowAnyOrigin()         
-//               .AllowAnyMethod()          
-//               .AllowAnyHeader();         
-//     });
-// });
+    
 
 builder.Services.AddCors(opt => opt.AddPolicy("MyPolicy", policy =>
 {
@@ -28,8 +30,6 @@ builder.Services.AddCors(opt => opt.AddPolicy("MyPolicy", policy =>
     policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); }
 
 ));
-
-
 
 var app = builder.Build();
 
@@ -39,10 +39,8 @@ app.UseCors("MyPolicy");
 app.MapGet("/",  (ToDoDbContext db) =>
      "Hello World!");
 
-
 app.MapGet("/tasks", async (ToDoDbContext db) =>
     await db.Tasks.ToListAsync());
-
 
 app.MapPost("/tasks", async (ToDoDbContext db,  TodoApi.Task task) =>
 {
@@ -57,7 +55,6 @@ app.MapPut("/tasks/{id}", async (ToDoDbContext db, int id,  TodoApi.Task updated
     var task = await db.Tasks.FindAsync(id);
     if (task == null) return Results.NotFound();
 
-    //task.Name = updatedTask.Name;
     task.IsComplete = updatedTask.IsComplete;
 
     await db.SaveChangesAsync();
